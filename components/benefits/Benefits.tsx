828b22a26f90c8d3cdbba83ffbc380e3c7b45df8
Husni <husniwfayo@gmail.com>
2026-01-07 02:01:15 +0530

    updated

--- components/benefits/Benefits.tsx
+++ components/benefits/Benefits.tsx
@@ -7,18 +7,16 @@ const Benefits = () => {
   return (
     <div className='min-h-screen flex flex-col gap-9'>
         <div className='flex justify-center items-center'>
-            <header 
-                className='text-center border w-fit px-4 py-2 rounded-lg'>
-                Benifits
+            <header className='text-center border w-fit px-4 py-2 rounded-lg text-lg'>
+              Benefits
             </header>
         </div>
         <div className='flex flex-col gap-6'>
-            <header 
-                className='text-center font-bold text-3xl md:text-4xl lg:text-5xl'>
-                    Membership Benefits
+            <header className='text-center font-bold text-3xl md:text-4xl lg:text-5xl'>
+              Membership Benefits
             </header>
             <p className='text-center'>
-                Thoughtfully crafted perks that support long-term growth.
+              Thoughtfully crafted perks that support long-term growth.
             </p>
         </div>
         <div className='grid grid-cols-1 md:grid-cols-3 gap-3'>
