8e55ca22719c64bad9cbeb82105aeddd5aa5a711
Husni <husniwfayo@gmail.com>
2026-01-07 01:40:46 +0530

    added resusable links and founder image

---
+++ lib/constants/legal.ts
@@ -0,0 +1,22 @@
+export const legal = [
+    { 
+        id: 1, 
+        href: "", 
+        label: "Privacy Policy" 
+    },
+    { 
+        id: 2, 
+        href: "", 
+        label: "Terms of Service" 
+    },
+    { 
+        id: 3, 
+        href: "", 
+        label: "Subscription Terms" 
+    },
+    { 
+        id: 4, 
+        href: "", 
+        label: "Copyright Policy" 
+    }
+]
