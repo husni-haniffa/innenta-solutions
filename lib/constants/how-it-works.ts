869cbf304013ea0b7366796adf49aa1a8ef95917
Husni <husniwfayo@gmail.com>
2026-01-07 02:23:06 +0530

    added how it works section

---
+++ lib/constants/how-it-works.ts
@@ -0,0 +1,22 @@
+import { Rocket, Sparkles, Layers } from "lucide-react"
+
+export const howItWorks = [
+    { 
+        id: 1, 
+        icon: Rocket, 
+        title: "Subscribe & Request", 
+        description: "Subscribe to a plan & enjoy unlimited design requests." 
+    },
+    { 
+        id: 2, 
+        icon: Sparkles, 
+        title: "Receive & Revisions", 
+        description: "Get high-quality designs and revise until you’re 100% satisfied!" 
+    },
+    { 
+        id: 3, 
+        icon: Layers, 
+        title: "Focus & build!", 
+        description: "Focus on your startup while we get all the creative work done." 
+    }
+]
