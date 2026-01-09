3979d4f462bab2df89f27f321cd8bcec806fcb80
Husni <husniwfayo@gmail.com>
2026-01-09 22:50:48 +0530

    removed spaces

--- components/services/Services.tsx
+++ components/services/Services.tsx
@@ -54,18 +54,17 @@ const Services = () => {
             }}
             className='flex flex-col gap-6 px-4 py-6 rounded-xl border hover:border-blue-500'
           > 
-           <div className='flex items-center gap-3'>
-  <service.icon size={50} className='text-blue-500' />
-  <header className='text-2xl font-bold text-center'>{service.title}</header>
-</div>
-
-<div className='grid grid-cols-1 md:grid-cols-2 gap-2'>
-  {service.items.map((item) => (
-    <div key={item.id} className='border rounded flex justify-center items-center py-2 px-2'>
-      <p className='text-sm text-muted-foreground text-center'>{item.title}</p>
-    </div>
-  ))}
-</div>
+            <div className='flex items-center gap-3'>
+              <service.icon size={50} className='text-blue-500' />
+              <header className='text-2xl font-bold text-center'>{service.title}</header>
+            </div>
+            <div className='grid grid-cols-1 md:grid-cols-2 gap-2'>
+              {service.items.map((item) => (
+                <div key={item.id} className='border rounded flex justify-center items-center py-2 px-2'>
+                  <p className='text-sm text-muted-foreground text-center'>{item.title}</p>
+                </div>
+              ))}
+            </div>
           </motion.div>
         ))}
       </motion.div>
